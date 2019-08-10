98120ced1d5fc93bd9d601305991037b1f5d1a3a
xiewo13 <52237434+xiewo@users.noreply.github.com>
2019-08-10 18:26:16 +0800

    Add files via upload

---
+++ baby.vue
@@ -0,0 +1,77 @@
+<template>
+    <div id='d2'>
+        <h2>我们都是木头人</h2>
+  
+       <ul>
+          <li> <img src="./img/photo.png" alt=""></li>
+          <li> <img src="./img/record.png" alt=""></li>
+          <li> <img src="./img/weight.png" alt=""></li>
+          <li> <img src="./img/height.png" alt=""></li>
+          <li> <img src="./img/first.png" alt=""></li>
+          <li> <img src="./img/sick.png" alt=""></li>
+          <li> <img src="./img/wounded.png" alt=""></li>
+          <li> <img src="./img/sleep.png" alt=""></li>
+          <li> <img src="./img/breast.png" alt=""></li>
+          <li> <img src="./img/feed.png" alt=""></li>
+          <li> <img src="./img/formula.png" alt=""></li>
+          <li> <img src="./img/rice.png" alt=""></li>
+          <li> <img src="./img/nappy.png" alt=""></li>
+          <li> <img src="./img/sleep.png" alt=""></li>
+      </ul> 
+   
+    </div>
+       
+
+
+
+</template>
+
+
+
+<script>
+export default {
+    data(){
+        return{
+        
+        }
+    },
+    methods:{
+
+    },
+    match:{
+
+    },
+}
+</script>
+
+
+
+<style>
+*{
+    margin:0;padding:0;
+}
+
+#d2{
+   
+ 
+       
+}
+
+#d2 ul{
+     
+    
+}
+
+#d2 ul li{
+    float:left;
+    list-style:none;
+    width:120px;
+    line-height:120px;
+    text-align:center;
+    border:1px solid #ddd;
+}
+
+ #d2 img{
+     
+  }
+</style>
