39549ba93d1e8c431f77ac13db0380914827c4f8
xiewo13 <52237434+xiewo@users.noreply.github.com>
2019-08-12 20:10:43 +0800

    Add files via upload

--- baby.vue
+++ baby.vue
@@ -1,29 +1,66 @@
 <template>
-    <div id='d2'>
+    <div class='nav'>
         <h2>我们都是木头人</h2>
   
-       <ul>
-          <li> <img src="./img/photo.png" alt=""></li>
-          <li> <img src="./img/record.png" alt=""></li>
-          <li> <img src="./img/weight.png" alt=""></li>
-          <li> <img src="./img/height.png" alt=""></li>
-          <li> <img src="./img/first.png" alt=""></li>
-          <li> <img src="./img/sick.png" alt=""></li>
-          <li> <img src="./img/wounded.png" alt=""></li>
-          <li> <img src="./img/sleep.png" alt=""></li>
-          <li> <img src="./img/breast.png" alt=""></li>
-          <li> <img src="./img/feed.png" alt=""></li>
-          <li> <img src="./img/formula.png" alt=""></li>
-          <li> <img src="./img/rice.png" alt=""></li>
-          <li> <img src="./img/nappy.png" alt=""></li>
-          <li> <img src="./img/sleep.png" alt=""></li>
-      </ul> 
-   
+       <ul  class='nav-item'>
+          <li class='item-li'>
+              <a href="javascript:;"><img src="./img/photo.png" alt=""></a>
+               <p>照片图片</p>  
+          </li>
+          <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/record.png" alt=""></a>
+               <p>记录</p>  
+          </li>
+          <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/weight.png" alt=""></a>
+               <p>体重</p>  
+          </li>
+          <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/height.png" alt=""></a>
+               <p>身高</p>  
+          </li>
+          <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/first.png" alt=""></a>
+               <p>第一次</p>  
+          </li>
+          <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/sick.png" alt=""></a>
+               <p>生病</p>  
+          </li>
+          <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/wounded.png" alt=""></a>
+               <p>受伤</p>  
+          </li>
+          <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/sleep.png" alt=""></a>
+               <p>宝宝语录</p>  
+          </li>
+          <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/breast.png" alt=""></a>
+               <p>亲喂</p>  
+          </li>
+          <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/feed.png" alt=""></a>
+               <p>瓶喂</p>  
+          </li>
+          <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/formula.png" alt=""></a>
+               <p>配方奶</p>  
+          </li>
+           <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/rice.png" alt=""></a>
+               <p>辅食</p>  
+          </li>
+           <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/nappy.png" alt=""></a>
+               <p>尿布</p>  
+          </li>
+          <li  class='item-li'>
+              <a href="javascript:;"><img src="./img/sleep.png" alt=""></a>
+               <p>睡觉</p>  
+          </li>
+        </ul> 
     </div>
-       
-
-
-
 </template>
 
 
@@ -50,28 +87,35 @@ export default {
 *{
     margin:0;padding:0;
 }
-
-#d2{
-   
- 
-       
+.nav{
+    width:375px; 
+    height:100%;
+    margin:0 auto;
 }
 
-#d2 ul{
-     
-    
+ul.nav-item{
+     width:375px; 
+    margin:0 auto;
 }
 
-#d2 ul li{
+ul.nav-item  .item-li{
     float:left;
     list-style:none;
-    width:120px;
-    line-height:120px;
+    width:125px;
+    height:120px;
     text-align:center;
-    border:1px solid #ddd;
+    border-bottom:1px solid #ddd;
+    border-right:1px solid #ddd;
+    box-sizing:border-box;
+    padding:8px;
+} 
+li.item-li img{
+    width: 60px;
+    height:60px; 
+    padding:5px;
 }
-
- #d2 img{
-     
-  }
+li.item-li p{
+    margin-top:5px;
+}
+ 
 </style>
